f95bece8a943b2cd33ba9f92190c0455e6ceb0a9
Pedro Liska <pedroliska@gmail.com>
2016-10-06 13:33:58 -0600

    renaming AppComponent to MoviesComponent

--- MovieFinder/app/app.module.ts
+++ MovieFinder/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { MoviesComponent }  from './movies.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  declarations: [ MoviesComponent ],
+  bootstrap: [ MoviesComponent ]
 })
 export class AppModule { }
