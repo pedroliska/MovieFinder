4faa44f468d389d3c53aafadab6bc59daffc67d8
Pedro Liska <pedroliska@gmail.com>
2016-10-17 15:37:12 -0600

    getting the jsonp branch to work again

--- MovieFinder/app/app.module.ts
+++ MovieFinder/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { JsonpModule }   from '@angular/http';
 
 import { MoviesComponent }  from './movies/movies.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, JsonpModule ],
   declarations: [ MoviesComponent ],
   bootstrap: [ MoviesComponent ]
 })
