6119dcf70a6e62a6defbfc796b90cc9d4a4afe0b
Pedro Liska <pedroliska@gmail.com>
2017-04-06 20:14:01 -0600

    removing dead code

--- angular2/MovieFinder/app/movies/rotten-tomatoes.service.ts
+++ angular2/MovieFinder/app/movies/rotten-tomatoes.service.ts
@@ -7,19 +7,8 @@ import 'rxjs/add/operator/catch';
 
 import { IMovie }   from './movie';
 
-// This service throttles the hits we do on the rottentomatoes API.
-// This used to be important with their public API, but they wanted
-// me to pay for it. Now that I'm using the private API it probably
-// doesn't matter but I'm leaving the throttling here because it 
-// works. I might remove it if it gets in the way.
 @Injectable()
 export class RottenTomatoesService {
-    private queueIsBeingProcessed = false;
-    private requestQueue: IRequest[] = [];
-    private beganCountingOn: number = null;
-    private maxHitsPerSecond = 5;
-    private hitCounter = 0;
-    private debug = true;
 
     constructor(private http: Http) { }
 
@@ -54,62 +43,8 @@ export class RottenTomatoesService {
             .subscribe(withJsonFn);
     }
 
-    private consoleOut(...args: any[]): void {
-        if (this.debug) {
-            console.log.apply(console, args);
-        }
-    }
-
-    getMilisecondsSinceEpoch(): number {
-        return new Date().getTime();
-    };
-
-    // recursive function. will process only one item
-    // each time it is called but it will call itself
-    // until there are no more items to process
-    private processQueue(): void {
-        this.queueIsBeingProcessed = true;
-        this.consoleOut('processQueue start', this.getMilisecondsSinceEpoch(), this.beganCountingOn, this.hitCounter);
-        if (this.requestQueue.length === 0) {
-            this.consoleOut('queue is empty');
-            this.queueIsBeingProcessed = false;
-            return;
-        }
-        if (!this.beganCountingOn) {
-            this.beganCountingOn = this.getMilisecondsSinceEpoch();
-        }
-        // timeout processQueue if we have reached the maxHitsPerSecond
-        if (this.hitCounter >= this.maxHitsPerSecond) {
-            const milisecondsToTimeout = 1000;
-            this.beganCountingOn = null;
-            this.hitCounter = 0;
-            this.consoleOut('reset counters');
-            if (milisecondsToTimeout > 0) {
-                this.consoleOut(`timing out for ${milisecondsToTimeout}ms`);
-                window.setTimeout(this.processQueue, milisecondsToTimeout);
-                return;
-            }
-            this.consoleOut('no need to timeout');
-        }
-
-        this.hitCounter++;
-        const queueItem = this.requestQueue.shift();
-
-        this.consoleOut('fetching url');
-        this.http
-            .get(queueItem.url)
-            .map((resp) => { return resp.json() })
-            .catch(this.handleError)
-            .subscribe(queueItem.withJsonFn);       this.processQueue();
-    }
-
     private handleError(error: any) {
         console.error(error);
         return Observable.throw(error.json().error || 'Server error');
     }
-}
-
-interface IRequest {
-    url: string;
-    withJsonFn: (json: any) => void;
 }
