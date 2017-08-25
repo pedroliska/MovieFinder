bb3775d3d98a36e9e54d06feea82023d162d2bdb
Pedro Liska <pedroliska@gmail.com>
2017-08-24 22:00:00 -0600

    copying all files to dist folder. just need to tweak index.html js references

--- angular2/publish-angular.sh
+++ angular2/publish-angular.sh
@@ -7,7 +7,7 @@ rm -rf src/app/*.js
 echo "clear out transpiled .map files"
 rm -rf src/app/*.map
 
-echo "#clear out dist folder"
+echo "clear out dist folder"
 rm -rf dist
 
 echo "transpile .ts files"
@@ -15,3 +15,15 @@ npm run build
 
 echo "sytemjs-builder"
 node systemjs-builder.js
+
+echo "copying resources folder"
+cp -r src/resources/ dist/
+
+echo "copying app folder"
+cp -r src/app/ dist/
+
+echo "removing all but .html files from app folder"
+find dist/app/ -type f ! -name '*.html' -delete
+
+echo "copying index.html"
+cp -r src/index.html dist/
