54c5c55b99104233276fe8e2fc3c2894faaa25c2
Pedro Liska <pedroliska@gmail.com>
2017-08-24 22:35:33 -0600

    dealing with the top-rentals.aspx proxy when creating the dist folder

--- angular2/dist-create.sh
+++ angular2/dist-create.sh
@@ -16,14 +16,16 @@ npm run build
 echo "executing dist-systemjs-builder.js"
 node dist-systemjs-builder.js
 
+echo "copying app folder"
+cp -r src/app/ dist/
+echo "removing all but .html files from app folder"
+find dist/app/ -type f ! -name '*.html' -delete
+
 echo "copying resources folder"
 cp -r src/resources/ dist/
 
-echo "copying app folder"
-cp -r src/app/ dist/
-
-echo "removing all but .html files from app folder"
-find dist/app/ -type f ! -name '*.html' -delete
+echo "copying top-rentals.aspx"
+cp -r src/top-rentals.aspx dist/
 
 echo "copying index.html"
 cp -r src/index.html dist/
