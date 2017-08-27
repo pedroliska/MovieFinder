ff01069141c77c982424c10a064d0a6087ddbc98
Pedro Liska <pedroliska@gmail.com>
2017-08-26 23:06:22 -0600

    created mpaa rating filter, still need UI to display when we filtered all movies out

--- angular2/src/app/movies/movies.component.ts
+++ angular2/src/app/movies/movies.component.ts
@@ -7,7 +7,7 @@
 import { Component } from '@angular/core';
 
 import { Title } from '@angular/platform-browser';
-import { MovieFieldsService, IMovieField } from './movie-fields.service';
+import { MovieFieldsService, IMovieField, StringDict } from './movie-fields.service';
 import { RottenTomatoesService } from './rotten-tomatoes.service';
 import { TmdbService } from './tmdb.service';
 import { MyHttpService } from './my-http.service';
@@ -28,6 +28,11 @@ export class MoviesComponent {
     moviesAll: IMovie[] = [];
     moviesForUi: IMovie[] = [];
     sortFields: IMovieField[]; 
+    isHidingMoviesWithNoRating: boolean = true;
+
+    mpaaRatingShownDict: StringDict<boolean> = {};
+    allMpaaRatings: string[];
+
 
     constructor(
         private titleService: Title,
@@ -44,7 +49,13 @@ export class MoviesComponent {
         this.rotten.getTopRentals(movies => {
             this.moviesAll = movies;
             //this.moviesAll = _.take(movies, 6);
-            this.filterAndSortMovies(true);
+
+            this.allMpaaRatings = (<string[]>_(movies).map('mpaaRating').uniq().sort().value());
+            this.allMpaaRatings.forEach(r => {
+                this.mpaaRatingShownDict[r] = true;
+            });
+
+            this.filterAndSortMovies();
             this.tmdb.enhanceMovies(this.moviesAll);
         });
     }
@@ -54,16 +65,27 @@ export class MoviesComponent {
         this.sortMovies(this.moviesForUi);
     }
 
-    onEmptyRatingsFilterChange(checked: boolean) {
-        this.filterAndSortMovies(checked);
+    onMpaaFilterChange(rating: string, checked: boolean) {
+        this.mpaaRatingShownDict[rating] = checked;
+        this.filterAndSortMovies();
     }
 
-    filterAndSortMovies(filterEmptyRatings: boolean) {
-        let filteredMovies: IMovie[] = filterEmptyRatings
+    onEmptyRatingsFilterChange(checked: boolean) {
+        this.isHidingMoviesWithNoRating = checked;
+        this.filterAndSortMovies();
+    }
+
+    filterAndSortMovies() {
+
+        // apply isHidingMoviesWithNoRating filter
+        let filteredMovies: IMovie[] = this.isHidingMoviesWithNoRating
             ? _.filter(
                 this.moviesAll,
                 m => m.audienceRating != null && m.criticsRating != null)
             : this.moviesAll;
+
+        // apply MPAA ratings filter
+        filteredMovies = _.filter(filteredMovies, m => this.mpaaRatingShownDict[m.mpaaRating]);
 
         this.sortMovies(filteredMovies);
     }
