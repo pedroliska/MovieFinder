00c697a3b034af63f75a61f11a38d9b2326f075c
Pedro Liska <pedroliska@gmail.com>
2017-09-04 22:10:21 -0600

    Genres looking ok but not working right since we're hitting the genres endpoint many times

--- angular2/src/app/movies/tmdb.service.ts
+++ angular2/src/app/movies/tmdb.service.ts
@@ -7,15 +7,17 @@ import { IMovie } from './movie';
 @Injectable()
 export class TmdbService {
 
+    genreDict: { [id: number]: string } = null;
+
     constructor(private myHttp: MyHttpService) { }
 
     enhanceMovies(movies: IMovie[]): void {
 
         console.log(movies.length);
-        let url = 'https://api.themoviedb.org/3/search/movie?api_key=7fab4a62931d29948d1d9942f6d84e21&query=';
+        let url = this.getFullUrl('/search/movie');
         movies.forEach(localMovie => {
             var movieUrl = url + encodeURIComponent(localMovie.title);
-            this.myHttp.fetchJson(movieUrl, (json: any) => {
+            this.myHttp.fetchJson(movieUrl, (json: IMoviesJson) => {
 
                 //// refine what TMDB returned.
                 // get only exact title matches
@@ -23,17 +25,54 @@ export class TmdbService {
                 // get the first result
 
                 var tmdbMovie = _(json.results)
-                    .filter((m: any) => m.title === localMovie.title)
+                    .filter((m: IMovieJson) => m.title === localMovie.title)
                     .head();
                 if (tmdbMovie) {
-                    localMovie.year = tmdbMovie.release_date.substring(0, 4);
-                    localMovie.genres.push.apply(localMovie.genres, tmdbMovie.genre_ids.map(this.genreIdToName));
+                    localMovie.year = Number(tmdbMovie.release_date.substring(0, 4));
+                    this.fetchAndAddMovieGenres(localMovie, tmdbMovie.genre_ids);
                 }
             });
         });
     }
 
-    private genreIdToName(id: number): string {
-        return id.toString();
+    private fetchAndAddMovieGenres(localMovie: IMovie, genreIds: number[]): void {
+        if (!this.genreDict) {
+            this.myHttp.fetchJson(this.getFullUrl('/genre/movie/list'), (json: IGenresJson) => {
+                this.genreDict = {};
+                json.genres.forEach(g => {
+                    this.genreDict[g.id] = g.name;
+                });
+                this.addMovieGenres(localMovie, genreIds);
+            });
+        } else {
+            this.addMovieGenres(localMovie, genreIds);
+        }
+    }
+
+    private addMovieGenres(localMovie: IMovie, genreIds: number[]): void {
+        localMovie.genres.push.apply(localMovie.genres, genreIds.map(id => this.genreDict[id]));
+    }
+    private getFullUrl(apiUrl: string) : string {
+        return 'https://api.themoviedb.org/3' + apiUrl + '?api_key=7fab4a62931d29948d1d9942f6d84e21&query=';
     }
 }
+
+interface IMoviesJson {
+    results: IMovieJson;
+}
+
+interface IMovieJson {
+    title: string;
+    release_date: string;
+    genre_ids: number[];
+}
+
+interface IGenresJson {
+    genres: IGenreJson[];
+}
+
+interface IGenreJson {
+    id: number;
+    name: string;
+}
+
