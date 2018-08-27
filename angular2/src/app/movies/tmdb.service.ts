4ee4f1ebbfb9e2554ad7634b86f8439a6f86596f
Pedro Liska <pedroliska@gmail.com>
2018-08-26 23:09:09 -0600

    making it so if tmdb only returns one match, we just use that one

--- angular2/src/app/movies/tmdb.service.ts
+++ angular2/src/app/movies/tmdb.service.ts
@@ -24,14 +24,21 @@ export class TmdbService {
                 var movieUrl = url + encodeURIComponent(localMovie.title);
                 this.myHttp.fetchJson(movieUrl, (json: IMoviesJson) => {
 
-                    //// refine what TMDB returned.
-                    // get only exact title matches
-                    // sort by relese date desc (not needed since that's the sort that comes from TMDB)
-                    // get the first result
+                    let movieResults: IMovieJson[] = json.results;
+                    var tmdbMovie: IMovieJson;
+                    if (movieResults.length === 1) {
+                        tmdbMovie = movieResults[0];
+                    } else {
 
-                    var tmdbMovie = _(json.results)
-                        .filter((m: IMovieJson) => m.title.toLowerCase() === localMovie.title.toLowerCase())
-                        .head();
+                        //// refine what TMDB returned.
+                        // get only exact title matches
+                        // sort by relese date desc (not needed since that's the sort that comes from TMDB)
+                        // get the first result
+
+                        tmdbMovie = _(json.results)
+                            .filter((m: IMovieJson) => m.title.toLowerCase() === localMovie.title.toLowerCase())
+                            .head();
+                    }
                     if (tmdbMovie) {
                         localMovie.year = Number(tmdbMovie.release_date.substring(0, 4));
                         localMovie.genres.push.apply(localMovie.genres, tmdbMovie.genre_ids.map(id => genreDict[id]));
@@ -41,13 +48,13 @@ export class TmdbService {
         });
     }
 
-    private getFullUrl(apiUrl: string) : string {
+    private getFullUrl(apiUrl: string): string {
         return 'https://api.themoviedb.org/3' + apiUrl + '?api_key=7fab4a62931d29948d1d9942f6d84e21&query=';
     }
 }
 
 interface IMoviesJson {
-    results: IMovieJson;
+    results: IMovieJson[];
 }
 
 interface IMovieJson {
