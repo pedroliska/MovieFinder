77a2c3e81238bc843973aee3f1e89425180bb3f5
Pedro Liska <pedroliska@gmail.com>
2019-12-05 12:14:14 -0700

    sorting by critics rating by default

--- angular8/src/app/movie-fields.service.ts
+++ angular8/src/app/movie-fields.service.ts
@@ -9,8 +9,8 @@ export class MovieFieldsService {
 
     constructor() {
         let simpleFields = [
-            ['audienceRating', 'Audience Rating', true],
             ['criticsRating', 'Critics Rating', true],
+            ['audienceRating', 'Audience Rating', true],
             ['rank', 'Rental Rank', false],
             ['year', 'Year', true],
             ['mpaaRating', 'MPAA Rating', true]
